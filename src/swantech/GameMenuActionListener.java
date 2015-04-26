ba81678c48f2a73c735b6ffc618462e5c18f8a32
Simon Hewitt <simon.d.hewitt@gmail.com>
2015-04-26 16:12:10 +0100

    Added BSD license

--- src/swantech/GameMenuActionListener.java
+++ src/swantech/GameMenuActionListener.java
@@ -14,14 +14,25 @@ public class GameMenuActionListener implements ActionListener {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()){
-            case "New game":
-                GameBoard.getGameBoardInstance().GameOptionsDialog();//start game
-                //AppGame.GameInstance().GetGameBoardInstance().GameOptionsDialog(); //start game
-                break;
-            case "Exit":
-                //TODO create a handler for this
-                break;
+        String cmd = new String((e.getActionCommand()));
+        if (cmd.equals("New Game")){
+            GameBoard.getGameBoardInstance().GameOptionsDialog();//start game
+        } else if (cmd.equals("Exit")){
+            System.out.println("EXIT");
+            // todo: add exit code
+        } else {
+            System.err.println("GamemenuActionListener:actionPerformed Unknown option");
         }
+
+        // This code will not run under OS X InterrliJ - stuck with JVM 1.6!
+//        switch (e.getActionCommand()){
+//            case "New game":
+//                GameBoard.getGameBoardInstance().GameOptionsDialog();//start game
+//                //AppGame.GameInstance().GetGameBoardInstance().GameOptionsDialog(); //start game
+//                break;
+//            case "Exit":
+//                //TODO create a handler for this
+//                break;
+//        }
     }
 }
